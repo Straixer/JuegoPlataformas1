168be665d040a4c1ce56da7cb3f56868170c67a6
Straixer <christiandmf@gmail.com>
2026-01-23 23:50:51 +0100

    Inicio
    Movimiento inicial

---
+++ Assets/movimiento.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class movimiento : MonoBehaviour
+{
+    public GameObject Player;
+    public float velocity = 1;
+    public Rigidbody rg;
+    public float jumpforce = 1;
+
+    BoxCollider box;
+    bool canJump = true;
+    SphereCollider sp;
+    public GameObject suelo;
+
+    Transform transform;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        box = suelo.GetComponent<BoxCollider>();
+        sp = Player.GetComponent<SphereCollider>();
+        transform = Player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKey(KeyCode.W)){
+            transform.position = new Vector3(transform.position.x + velocity*Time.deltaTime, transform.position.y, transform.position.z);
+        }
+        if(Input.GetKey(KeyCode.S)){
+            transform.position = new Vector3(transform.position.x - velocity*Time.deltaTime, transform.position.y, transform.position.z);
+        }
+        if(Input.GetKey(KeyCode.A)){
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + velocity*Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.D)){
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - velocity*Time.deltaTime);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space)&&canJump){
+            rg.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.tag == "Suelo")
+        {
+            canJump=false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Suelo")
+        {
+            canJump=true;
+        }
+    }
+}
+
+
