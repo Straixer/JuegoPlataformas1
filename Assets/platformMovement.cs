827b3a6900d56acf4fe133500b6d1ff6c935da43
Straixer <christiandmf@gmail.com>
2026-01-30 17:06:48 +0100

    Nuevo movimiento plataformas

--- Assets/platformMovement.cs
+++ Assets/platformMovement.cs
@@ -6,28 +6,57 @@ public class platformMovement : MonoBehaviour
     public float velocityPlatform1 = 1;
     public float waitTimePlatform1 = 2;
     public float distancePlatform1 = 3;
+    Vector3 initialPositionPlatform1;
     bool endPlatform1=false;
     Transform transformPlatform1;
+
+    public GameObject player;
+    Transform transformPlayer;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         transformPlatform1 = platform1.transform;
+        initialPositionPlatform1 = transformPlatform1.position;
+
+        transformPlayer = player.transform;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        float distanceFromStartPlatform1 = transformPlatform1.position.x - initialPositionPlatform1.x;
         if (endPlatform1) {
-            transformPlatform1.position = new Vector3(transform.position.x - velocityPlatform1* Time.deltaTime, transform.position.y, transform.position.z);
+            transformPlatform1.Translate(Vector3.right * velocityPlatform1 * Time.deltaTime);
+            if (distanceFromStartPlatform1 >= distancePlatform1)
+            {
+                endPlatform1 = false;
+            }
         }
         else
         {
-            transformPlatform1.position = new Vector3(transform.position.x + velocityPlatform1 * Time.deltaTime, transform.position.y, transform.position.z);
+            transformPlatform1.Translate(Vector3.left * velocityPlatform1 * Time.deltaTime);
+            if (distanceFromStartPlatform1 <= -distancePlatform1)
+            {
+                endPlatform1 = true;
+            }
         }
+    }
 
-        if(transformPlatform1.position.x>distancePlatform1|| transformPlatform1.position.x < distancePlatform1*-1)
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
         {
-            endPlatform1 = !endPlatform1;
+            if (endPlatform1)
+            {
+                transformPlayer.Translate(Vector3.right * velocityPlatform1 * Time.deltaTime);
+            }
+            else
+            {
+                transformPlayer.Translate(Vector3.left * velocityPlatform1 * Time.deltaTime);
+            }
         }
     }
 
